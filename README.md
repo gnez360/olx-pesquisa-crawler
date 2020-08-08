6d6f13efd715a022696ecf72429ec5a84b1f1bc5
gnez360 <39109584+gnez360@users.noreply.github.com>
2020-08-08 10:41:20 -0300

    atualização do readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # olx-crawler
  web crawler de monitoramento de preços OLX
+
+# Instalação
+habilitar extensão openssl no php.ini
+
+composer update
+
+
--- app/Http/Controllers/OlxController.php
+++ app/Http/Controllers/OlxController.php
@@ -21,14 +21,14 @@ class OlxController extends Controller
         //
     }
 
-    public function listitems() 
+    public function listitems()
     {
-        
-        $url = "https://mg.olx.com.br/belo-horizonte-e-regiao/computadores-e-acessorios/notebook-e-netbook?q=gtx&sf=1";
+
+        $url = "https://mg.olx.com.br/belo-horizonte-e-regiao/computadores-e-acessorios/notebook-e-netbook";
         $browser = new HttpBrowser(HttpClient::create());
 
-        $crawler = $browser->request('GET', $url);  
-       
+        $crawler = $browser->request('GET', $url);
+
             $content = $crawler->filter('div.col2.sc-15vff5z-5.fFdJjk')->each(function ($node) {
                 $res = $node->filter('div.fnmrjs-7.cfjFVu')->each(function ($row) {
                     $name = $row->filter('div.fnmrjs-8.kRlFBv')->each(function ($field) {
@@ -39,32 +39,32 @@ class OlxController extends Controller
                     } else {
                         $name = $name[0];
                     }
-    
+
                     $price = $row->filter('div.fnmrjs-15.clbSMi')->each(function ($field) {
                         return $field->text();
-                    }); 
-                    
+                    });
+
                     $date = $row->filter('p.fnmrjs-19.eJIIxH')->each(function ($field) {
                         return $field->text();
-                    }); 
-    
+                    });
+
                     $data['name'] = $name;
                     $data['price'] = $price;
                     $data['date'] = $date;
-    
+
                     return $data;
                 });
                 return $res;
             });
-            
+
             if(count($content) > 0) {
                 $result['status'] = "success";
+                $result['code'] = 200;
                 $result['data'] = $content[0];
             }else{
                 $result['status'] = "failed";
             }
-    
+
             return response()->json($result);
         }
     }
-    
